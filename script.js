29ffb2d46515098167fe70f5d837ea87f6df063a
currie04 <orlenamay2204@hotmail.com>
2024-09-07 23:08:28 +0100

    added ability to toggle hover effect with a double click, and added colour options - easy to expand, just add another colour to the button

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const container = document.querySelector("#container");
 const button = document.querySelector("#size");
 
 let numSquaresSide = 16;
+let colour = "green";
+let hoverEnabled = true; 
+
 
 
 function drawGrid(){
@@ -16,15 +19,53 @@ function drawGrid(){
         container.appendChild(box);
 
         box.addEventListener("mouseenter", function(e){
-            e.target.style.backgroundColor = "blue";
+            if(hoverEnabled){
+                e.target.style.backgroundColor = colour;
+            }
         });
     }
 }
 
-button.addEventListener("click", function(){
+function changeGridSize(){
     numSquaresSide = parseInt(prompt("Please enter how many squares per side"));
+
+    if(numSquaresSide > 100){
+       alert("Please enter a lower number");
+       changeGridSize();
+    }
+
     container.replaceChildren(); 
     drawGrid(numSquaresSide);
+}
+
+
+button.addEventListener("click", function(){
+    changeGridSize();
 });
 
+/* for colour changing */
+
+const button_div = document.querySelector("#buttons");
+const colour_buttons = button_div.querySelectorAll("button");
+
+colour_buttons.forEach(button => {
+    button.addEventListener("click", function(){
+        colour = button.id;
+    });
+})
+
+
+/* Toggle hover effect on double click */
+
+document.addEventListener("dblclick", function () {
+    hoverEnabled = !hoverEnabled; // Toggle the flag
+    console.log(`Hover effects ${hoverEnabled ? 'enabled' : 'disabled'}`);
+});
+
+
+
+
 drawGrid(16);
+
+
+
