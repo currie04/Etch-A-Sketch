1a110a625dd5f85ff5de8886c2329983a1d30fff
currie04 <orlenamay2204@hotmail.com>
2024-09-07 22:27:51 +0100

    added functionality to change grid size

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
 const container = document.querySelector("#container");
+const button = document.querySelector("#size");
 
-for( i = 0; i < 16* 16; i++){
-    const box = document.createElement("div");
-    box.classList.add("gridbox");
-    container.appendChild(box);
+let numSquaresSide = 16;
 
-    box.addEventListener("mouseenter", function(e){
-        e.target.style.backgroundColor = "blue";
-    });
+
+function drawGrid(){
+
+    const squareSize = container.clientWidth / numSquaresSide; //calculate size
+
+    for( i = 0; i < numSquaresSide * numSquaresSide; i++){
+        const box = document.createElement("div");
+        box.classList.add("gridbox");
+        box.style.width = `${squareSize}px`; // Set width
+        box.style.height = `${squareSize}px`; // Set height
+        container.appendChild(box);
+
+        box.addEventListener("mouseenter", function(e){
+            e.target.style.backgroundColor = "blue";
+        });
+    }
 }
+
+button.addEventListener("click", function(){
+    numSquaresSide = parseInt(prompt("Please enter how many squares per side"));
+    container.replaceChildren(); 
+    drawGrid(numSquaresSide);
+});
+
+drawGrid(16);
